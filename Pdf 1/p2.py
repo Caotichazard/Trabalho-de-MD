6f90881975a98c33772730b601e924c3b98e1b5c
CaoticHazard <guilsalomao@gmail.com>
2019-07-06 17:45:22 -0300

    Textos dos problemas do PDF 1 feitos

--- Pdf 1/p2.py
+++ Pdf 1/p2.py
@@ -1,9 +1,21 @@
-perimetro = 1000
-solucao = 0
-for a in range(1,perimetro+1):
-	for b in range(a+1,perimetro+1):
-		c = perimetro -a -b
-		if a*a + b*b == c*c:
-			solucao = a*b*c
+Problema: Special Pyhtagorian triplet
 
-print(solucao)
+Metodologia:
+	Uma tripla pitagorica é quando a soma dos quadrados de dois numeros é igual ao quadrado de um terceiro número, ou seja a²+b²=c².Para resolver esse problema foi usada a logica de que, C tem que ser o complemento da soma de A e B, e então de forma interativa foi testada as possibilidades dessa combinação
+
+
+Resultados e discussões:
+	soma = 1000#esse é o resultado da soma dos 3 numeros
+	solucao = 0
+	for a in range(1,soma+1):#Sera testada cada iteração a partir do caso base de A ser 1 ate A ser 1000 
+		for b in range(a+1,soma+1):#então sera testado o B>A que então consiga satisfazer a solução
+			c = soma - a - b
+			if a*a + b*b == c*c:
+				solucao = a*b*c
+
+	print(solucao)
+	A saida do programa foi 31875000, compativel com a resposta no arquivo fornecido.
+
+Considerções finais:
+	A solução desse problema foi difícil de se traduzir para codigo poís há varias soluções possiveis porém cada uma tem sua dificuldade de implementação, entretanto, a "ideia" da solução foi facil de se chegar.
+
--- Pdf 1/p3.py
+++ Pdf 1/p3.py
@@ -1,22 +1,35 @@
-start = 1
-longest_number = 1
-longest_steps = 1
-cur_steps = 0
-cur_number = 0
-while(start < 1000000):
-	cur_steps = 0
-	start = start + 2
-	cur_number = start
-	while (cur_number != 1):
-		if cur_number%2 == 0:
-			cur_number = cur_number/2
-			cur_steps = cur_steps+1
-		else:
-			cur_number = (3*cur_number)+1			
-			cur_steps = cur_steps+1
-	if cur_steps > longest_steps:
-		longest_number = start
-		longest_steps = cur_steps
-	
+Problema: Longest Collatz sequence
 
-print(longest_number)
+Metodologia:
+	Uma sequencia collatz é uma sequecia iterativa onde considerando a sequencia iniciando em um numero N, se N for par então n=n/2, se N for impar então, N=3N+1, até chegar ao caso base de N=1.Para resolver esse problema foi uma aplicação direta dessa definiçãoe então atualizar qual seria a maior sequencia sempre que uma maior for encontrada
+
+Resultados e Discussões:
+	
+	start = 1#começando pelo caso inicial
+	longest_number = 1#Caso inicial da mais longa
+	longest_steps = 1#Quantos passos ocorreram na mais longa
+	cur_steps = 0#contador de passos
+	cur_number = 0#numero inicial atual
+	while(start < 1000000):#verificando todos os casos ate 1000000
+		cur_steps = 0
+		start = start + 2#verifica apenas os numeros impares pois esse tem as sequencias mais longas
+		cur_number = start
+		while (cur_number != 1):#loop ocorre enquanto não atingir o caso base
+			if cur_number%2 == 0:
+				cur_number = cur_number/2
+				cur_steps = cur_steps+1
+			else:
+				cur_number = (3*cur_number)+1			
+				cur_steps = cur_steps+1
+		if cur_steps > longest_steps:#atualiza se a quantidade de passos da sequencia atual for maior que a sequencia maior anterior
+			longest_number = start
+			longest_steps = cur_steps
+		
+
+	print(longest_number)
+
+	O retorno desse algoritmo foi de 837799, igual ao resultado fornecido no arquivo de respostas
+
+Considerações finais:
+	A solução desse problema foi direta porem achar detalhes que a otimizaçem foi dificil, a unica opção de otimização que pude encontrar foi a de pular os numeros pares pois estes sempre retornavam as sequencias mais curtas
+
