ff1d04517dcf07875668e2409a0a6499f8b100c9
CaoticHazard <guilsalomao@gmail.com>
2019-07-06 18:08:17 -0300

    Problemas 2 e 3 do pdf 2 com textos

--- Pdf 2/Problem12.c
+++ Pdf 2/Problem12.c
@@ -1,37 +1,49 @@
-#include<stdlib.h>
-#include<stdio.h>
+Problema: Highly divisible triangular number
 
-int achaNumeroTriangular(int pos, int num_anterior);
-int achaQtdDivisores(int num);
+Metodologia:
+	Um número triangular na posição N é a soma de todos os naturais menores que N, pensando nisso, foi necessário criar uma forma para encontrar o número triangular de posição N, e então criar uma forma de testar seus divisores.
+
+Resultados e discussões:
+
+	#include<stdlib.h>
+	#include<stdio.h>
+
+	int achaNumeroTriangular(int pos, int num_anterior);//Função que encontra o número de posição N utilizando o numero da posição N-1
+	int achaQtdDivisores(int num);//Função que acha o numero de divisores de um número
 
 
-int main(){
-    int divisores=0;
-    int pos_atual = 0;
-    int num_atual=0;
-    while (divisores <500)
-    {   
-        
-        num_atual = achaNumeroTriangular(pos_atual,num_atual);
-        divisores = achaQtdDivisores(num_atual);
-        
-        pos_atual++;
-    }
-    printf("\n resultado: %d\n",num_atual);
-    
-    
-}
+	int main(){
+	    int divisores=0;//contador de divisores
+	    int pos_atual = 0;//posição atual a ser considerada
+	    int num_atual=0;//Numero triangular da posição atual
+	    while (divisores <500)// Realiza a operação ate o contador de divisores atinga 500 ou mais
+	    {   
+		
+		num_atual = achaNumeroTriangular(pos_atual,num_atual);
+		divisores = achaQtdDivisores(num_atual);
+		
+		pos_atual++;
+	    }
+	    printf("\n resultado: %d\n",num_atual);
+	    
+	    
+	}
 
-int achaNumeroTriangular(int pos, int num_anterior){
-    return pos+num_anterior;
-}
+	int achaNumeroTriangular(int pos, int num_anterior){
+	    return pos+num_anterior;
+	}
 
-int achaQtdDivisores(int num){
-    int cont=0;
-    for(int i=1;i<=num;i++){
-        if(num%i==0){
-            cont++;
-        }
-    }
-    return cont;
-}
+	int achaQtdDivisores(int num){
+	    int cont=0;
+	    for(int i=1;i<=num;i++){
+		if(num%i==0){//verifica se o resto da divisão é zero o que o caracteriza como divisor
+		    cont++;
+		}
+	    }
+	    return cont;
+	}
+
+	O retorno desse algoritmo foi de 76576500 correspondendo com a resposta fornecida
+
+Considerações finais:
+	A soluçao desse problema era bem direta e facil de se implementar, o que tornou viavel usar a linguagem C para isso pois ela poderia realizar em um menor tempo as operações em comparação a python. Além disso outra otimização foi o uso do numero anterior para calcular o numero da posição atual pois assim se tornava um operção simples o que acelerava o processo.
--- Pdf 2/p16.py
+++ Pdf 2/p16.py
@@ -1,15 +1,26 @@
-x = 2**1000
-div = 10
-aux = 0
-aux2 = 1
-soma = 0
-while(x>0):
+Problema: Power digit sum
 
-    aux = x%div
-    print(aux/aux2)
-    soma = soma+ (aux/aux2)
-    x = x - aux
-    aux2 =div
-    div = div*10
+Metodologia:
+	O objetivo era encontrar a soma de todos os digitos de 2^1000, portanto, uma maneira simples era dividir o numero por potencias de 10, somar o resto da divisão a soma dos digitos, e então subtrair esse resto do numero inicial, e então aumentar a potencia de 10. 
 
-print(soma)
+
+Resultados e discussões:
+
+	x = 2**1000#numero a ser usado
+	div = 10#potencia de 10 inicial
+	aux = 0
+	aux2 = 1
+	soma = 0#soma final dos digitos
+	while(x>0):#realiza o loop até que o numero inicial seja 0
+
+	    aux = x%div#armazena o digito a ser recebido nessa iteração
+	    soma = soma+ (aux/aux2)#soma o digito a soma total, é usado um auxiliar para garantir que o numero seja unitário 
+	    x = x - aux#remove o resto do número
+	    aux2 =div# atualiza as potencias para poder continuar a operação
+	    div = div*10
+
+	print(soma)
+	O resultado desse algoritmo foi 1366 igual ao resultado fornecido no arquivo de respostas
+
+Considerações finais:
+	A solução desse problema é simples porém é necessário uma estrutura muito flexível para comportar o número 2^1000,ja que esse requer cerca de 1000 bits para armazenar, então, ao contrario da minha decisão inicial, foi necessário o uso de python para essa operação.
